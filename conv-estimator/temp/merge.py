1c97783e01e38541ed2d15cee456c0719a81503b
nicfreeman1209 <nicfreeman1209@gmail.com>
2021-06-19 17:35:56 +0100

    initial commit

---
+++ conv-estimator/temp/merge.py
@@ -0,0 +1,19 @@
+import sys
+import glob
+from PIL import Image
+
+files = glob.glob("./*.png")
+images = [Image.open(x) for x in files]
+widths, heights = zip(*(i.size for i in images))
+
+total_width = sum(widths)
+max_height = max(heights)
+
+new_im = Image.new('RGB', (total_width, max_height))
+
+x_offset = 0
+for im in images:
+  new_im.paste(im, (x_offset,0))
+  x_offset += im.size[0]
+
+new_im.save('test.jpg')
